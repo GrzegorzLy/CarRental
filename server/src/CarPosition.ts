e24ba4fba33017c610fcb22a1820017819e79bec
Grzegorz Lyczak <grzegorz.lyczak@lookmove.ch>
2020-04-08 01:01:19 +0200

    car movement simulator

--- server/src/CarPosition.ts
+++ server/src/CarPosition.ts
@@ -1,13 +1,42 @@
+const SPEED = 0.0001;
+const SAFE_AREA = 0.012;
 export default class CarPosition {
-  lat: number;
-  lon: number;
-  brand: string;
   id: number;
+  lat: number;
+  lng: number;
+  brand: string;
+  private _lat: number;
+  private _lng: number;
+  private direction: Array<number>;
 
-  constructor(id: number, lat: number, lon: number, brand: string) {
+  constructor(id: number, lat: number, lng: number, brand: string) {
     this.id = id;
-    this.lat = lat;
-    this.lon = lon;
+    this.lat = lat - Math.random() * 0.05;
+    this.lng = lng - Math.random() * 0.05;
     this.brand = brand;
+    this._lat = lat;
+    this._lng = lng;
+    this.direction = [id % 2, (id + 1) % 2];
+  }
+
+  move() {
+    this.checkDirection();
+    this.lat += this.direction[0] ? -SPEED : SPEED;
+    this.lng += this.direction[1] ? -SPEED : SPEED;
+  }
+
+  private checkDirection() {
+    if (this.lng > this._lng + SAFE_AREA) {
+      this.direction[1] = 1;
+    }
+    if (this.lng < this._lng - SAFE_AREA) {
+      this.direction[1] = 0;
+    }
+    if (this.lat > this._lat + SAFE_AREA) {
+      this.direction[0] = 1;
+    }
+    if (this.lat < this._lat - SAFE_AREA) {
+      this.direction[0] = 0;
+    }
   }
 }
--- server/src/coords.ts
+++ server/src/coords.ts
@@ -1,15 +1,42 @@
 import CarPosition from './CarPosition';
+const COORDS_AMOUNT = 20;
 
-const coords: Array<CarPosition> = [
-  new CarPosition(1, 54.37598, 18.60835, 'polonez'),
-  new CarPosition(2, 54.37598, 18.60835, 'maluch'),
-  new CarPosition(3, 54.37598, 18.60835, 'uno'),
-];
+let coords: Array<CarPosition> = [];
+const clients: Array<string> = [];
+
+function createFakeCoords(lat = 54.37598, lng = 18.60835, amount = 10) {
+  if (coords.length) {
+    return;
+  }
+  coords = [
+    new CarPosition(1, lat, lng, 'polonoze caro'),
+    new CarPosition(2, lat, lng, 'maluch plus'),
+    new CarPosition(3, lat, lng, 'fiat uno'),
+    new CarPosition(4, lat, lng, 'passat 5'),
+    new CarPosition(5, lat, lng, 'maluch plus'),
+    new CarPosition(6, lat, lng, 'fiat multipla'),
+    new CarPosition(7, lat, lng, 'audi v8'),
+    new CarPosition(8, lat, lng, 'kia joice'),
+    new CarPosition(9, lat, lng, 'mercedes vaneo'),
+    new CarPosition(10, lat, lng, 'polonez'),
+  ];
+}
 
 export function init(io: SocketIO.Server) {
   io.on('connection', (client) => {
     console.log('connection opened');
+    if (clients.includes(client.id)) {
+      return;
+    } else {
+      clients.push(client.id);
+    }
 
-    setInterval(() => client.emit('coords', coords), 1000);
+    client.once('coordsFilter', ([lat, lng]) => {
+      createFakeCoords(lat, lng, COORDS_AMOUNT);
+      setInterval(() => {
+        coords.forEach((c) => c.move());
+        io.sockets.emit('coords', coords);
+      }, 500);
+    });
   });
 }
